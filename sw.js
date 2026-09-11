80642416bdbd8e3d95f215404a9327f734fd8800
acemybusiness <acemybusiness.app@gmail.com>
2026-09-11 08:09:33 -0400

    Use final detailed MSA launcher icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='matts-square-v80-domain-pwa-fix';
-const APP_SHELL=['./','./index.html','./shared.html','./msa-logo.png','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE_NAME='matts-square-v80-correct-msa-icon';
+const APP_SHELL=['./','./index.html','./shared.html','./msa-logo.png','./manifest.webmanifest','./icons/icon-192-msa-final.png','./icons/icon-512-msa-final.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
