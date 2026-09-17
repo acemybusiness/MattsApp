056881639c86aeddcaac0ac23f3c97c2021b5175
acemybusiness <acemybusiness.app@gmail.com>
2026-09-17 12:41:10 -0400

    Refresh cache for v80.7.1 cross-device sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='matts-square-v80-7-live-function-repair';
+const CACHE_NAME='matts-square-v80-7-1-cross-device-sync';
 const APP_SHELL=['./','./index.html','./shared.html','./msa-logo.png','./msa-app-icon.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
