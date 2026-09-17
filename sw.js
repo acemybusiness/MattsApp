beb6416e30f71eb14a91f04a5f2c52239883ec76
acemybusiness <acemybusiness.app@gmail.com>
2026-09-17 14:23:35 -0400

    Refresh cache for v80.7.3 Share Code sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='matts-square-v80-7-2-delete-everywhere';
+const CACHE_NAME='matts-square-v80-7-3-share-code-sync';
 const APP_SHELL=['./','./index.html','./shared.html','./msa-logo.png','./msa-app-icon.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
