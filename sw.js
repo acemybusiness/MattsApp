1fd394c1f0cff7d59bac52d93dfd6f6269c792c9
acemybusiness <acemybusiness.app@gmail.com>
2026-09-17 10:22:11 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='matts-square-v80-5-two-logo-spots-cleanup';
-const APP_SHELL=['./','./index.html','./shared.html','./msa-logo.png','./manifest.webmanifest'];
+const CACHE_NAME='matts-square-v80-6-full-install-icon';
+const APP_SHELL=['./','./index.html','./shared.html','./msa-logo.png','./msa-app-icon.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
